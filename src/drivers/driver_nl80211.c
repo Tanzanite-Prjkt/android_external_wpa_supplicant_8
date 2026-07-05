7cee4af77f23a0e1a6b5960aecee4203e8b5146e
Nathan <nathannxx@proton.me>
2026-07-05 19:54:45 +0700

    nl80211: Add an option to disable NL80211_WPA_VERSION_3
    * Setting NL80211_WPA_VERSION_3 can cause connection issues on devices
    with older kernel versions. Previously, this was handled by a hardcoded
    revert [1], which broke WPA3 compatibility globally for devices with newer
    kernels.
    
    [1]: https://github.com/web1n/android_external_wpa_supplicant_8/commit/220688d0768625ec4982ee3f7b924607486fcf78
    
    Add the following to the device makefile:
    $(call soong_config_set,wpa_supplicant_8,wifi_disable_wpa_version_3,true)
    
    Change-Id: I3f5cd0d224c2148dab0b99c7dfcff300c25712e2
    Signed-off-by: Nathan <nathannxx@proton.me>

--- src/drivers/driver_nl80211.c
+++ src/drivers/driver_nl80211.c
@@ -3841,6 +3841,9 @@ static int wpa_ver_supported(struct wpa_driver_nl80211_data *drv,
 		ver |= NL80211_WPA_VERSION_1;
 	if (proto & WPA_PROTO_RSN) {
 		if (drv->chip_vendor_id != OUI_BRCM) {
+#if defined(CONFIG_DISABLE_WPA_VERSION_3)
+			ver |= NL80211_WPA_VERSION_2;
+#else
 			/*
 			* NL80211_ATTR_SAE_PASSWORD is related and was added
 			* at the same time as NL80211_WPA_VERSION_3.
@@ -3850,6 +3853,7 @@ static int wpa_ver_supported(struct wpa_driver_nl80211_data *drv,
 				ver |= NL80211_WPA_VERSION_3;
 			else
 				ver |= NL80211_WPA_VERSION_2;
+#endif
 		} else {
 			ver |= NL80211_WPA_VERSION_2;
 		}
